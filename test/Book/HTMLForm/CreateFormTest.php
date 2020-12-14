fdad0314b4fbf451e01b9ffe3716da64e36c4ae7
ahonson ahonson <ahonson@gmail.com>
2020-12-14 16:30:51 +0000

    Adding new unit tests, total code coverage 88,52%

---
+++ test/Book/HTMLForm/CreateFormTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace artes\Book\HTMLForm;
+
+use Anax\DI\DIFactoryConfig;
+use Anax\Response\ResponseUtility;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Testclass.
+ */
+class CreateFormTest extends TestCase
+{
+    // Create the di container.
+    protected $di;
+
+    /**
+     * Prepare before each test.
+     */
+    protected function setUp()
+    {
+        global $di;
+
+        // Setup di
+        $di = new DIFactoryConfig();
+        $di->loadServices(ANAX_INSTALL_PATH . "/config/di");
+        $di->loadServices(ANAX_INSTALL_PATH . "/test/config/di");
+
+        // Use a different cache dir for unit test
+        $di->get("cache")->setPath(ANAX_INSTALL_PATH . "/test/cache");
+
+        $this->di = $di;
+    }
+
+    /**
+     * Test the route "index".
+     */
+    // public function testCallbackSubmit()
+    // {
+    //     // Setup the controller
+    //     $controller = new CreateForm($this->di);
+    //     // $controller->setDI($this->di);
+    //
+    //     // Test the controller action
+    //     $res = $controller->callbackSubmit();
+    //     $this->assertTrue($res);
+    // }
+
+    /**
+     * Test the route "index".
+     */
+    public function testCallbackSuccess()
+    {
+        // Setup the controller
+        $controller = new CreateForm($this->di);
+        // $controller->setDI($this->di);
+
+        // Test the controller action
+        $res = $controller->callbackSuccess();
+        $this->assertNull($res);
+    }
+}
